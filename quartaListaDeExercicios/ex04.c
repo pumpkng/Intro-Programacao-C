fbc271d71a7f5ca2bf587eb66ca7292e61c5f404
pumpkng <rafael.rufato.santos@gmail.com>
2026-06-05 02:42:02 -0300

    add mais exercícios

--- quartaListaDeExercicios/ex04.c
+++ quartaListaDeExercicios/ex04.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
-int main(
+void inverte(char[]);
+
+int main(){
+    SetConsoleOutputCP(65001);
+    char numero[10];
+
+    printf("\nNúmero: ");
+    fgets(numero,sizeof(numero),stdin);
+    numero[strlen(numero)-1] = '\0';
+
+    inverte(numero);
+
+    printf("\n%s", numero);
+
+    return 0;
+}
+
+void inverte(char numero[]){
+    char aux;
+    for(int i=0;i<=(strlen(numero)-1)/2;i++){
+        aux = numero[i];
+        numero[i] = numero[strlen(numero)-i-1];
+        numero[strlen(numero)-i-1] = aux;
+    }
+}
--- quartaListaDeExercicios/ex07.c
+++ quartaListaDeExercicios/ex07.c
@@ -4,7 +4,6 @@
 #include <windows.h>
 
 int palindromo(char[]);
-void corrige(char[]);
 
 int main(){
     SetConsoleOutputCP(65001);
@@ -14,24 +13,12 @@ int main(){
     fgets(texto,sizeof(texto),stdin);
     texto[strlen(texto)-1] = '\0';
 
-    corrige(texto);
-    
     if(palindromo(texto))
         printf("É palíndromo!");
     else
         printf("Não é palíndromo!");
 }
 
-void corrige(char texto[]){
-    for(int i=0;i<strlen(texto)-1;i++){
-        if(texto[i] >= 'A' && texto[i] <= 'Z'){
-            texto[i]+=32;
-        }
-        if(texto[i] >= 'a' && texto[i] <= 'z'){
-            
-        }
-    }
-}
 int palindromo(char texto[]){
     for(int i=0;i<strlen(texto)/2;i++){
         if(texto[i]!=texto[strlen(texto)-1-i]){
